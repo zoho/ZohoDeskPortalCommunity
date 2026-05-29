73d68c9e1a4b97f68bf513308ba9c455aee00021
Vinoth N <vinoth.nv@zohocorp.com>
2026-05-29 17:45:37 +0530

    4.6.2

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = Version("4.6.1")
+let version = Version("4.6.2")
 let package = Package(
     name: "ZohoDeskPortalCommunity",
     platforms: [
@@ -21,7 +21,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "ZohoDeskPortalCommunity", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalCommunity/\(version.description)/ZohoDeskPortalCommunity.zip", checksum: "8988026b5e6ec3b95409efe839e0c0426fcc3becc9e48340212048a561deb323"),
+        .binaryTarget(name: "ZohoDeskPortalCommunity", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalCommunity/\(version.description)/ZohoDeskPortalCommunity.zip", checksum: "0760aaa0fc3f9c02cf680dc76d07550f06fa7d231d2cedce5f82d4241ca44a8a"),
         
         .target(
             name: "ZohoDeskPortalCommunityPackage",
